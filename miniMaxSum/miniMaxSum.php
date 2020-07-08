775274d03b41c2e0e139e2d0ec202698100838b6
ThibautB <dencis@MacBook-Pro-de-Dencis.local>
2020-07-08 21:34:46 +0200

    done

---
+++ miniMaxSum/miniMaxSum.php
@@ -0,0 +1,13 @@
+function miniMaxSum($arr) 
+{
+    
+    $total = array_sum($arr);
+    $newsum = [];    
+    for ($i=0; $i < count($arr); $i++) {
+        $newsum[] = $total-$arr[$i];
+    }
+    $min = min($newsum);
+    $max = max($newsum);
+    
+    echo $min.' '.$max;
+}
